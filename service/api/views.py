5fa46976c778db84d987cbfc209663092acaa083
Ольга Тихобаева <otikhobaeva@MacBook-Air-Olga.local>
2023-11-14 11:54:11 +0700

    homework_1

--- service/api/views.py
+++ service/api/views.py
@@ -1,16 +1,50 @@
-from typing import List
+import os
+import random
+from typing import Dict, List
 
-from fastapi import APIRouter, FastAPI, Request
+from dotenv import find_dotenv, load_dotenv
+from fastapi import APIRouter, FastAPI, Request, Security
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
 
-from service.api.exceptions import UserNotFoundError
+from service.api.exceptions import AuthorizationError, ModelNotFoundError, UserNotFoundError
 from service.log import app_logger
+
+load_dotenv(find_dotenv())
+
+token_header = APIKeyHeader(name="Authorization")
+
+
+def get_api_key(api_key_header: str = Security(token_header)) -> str:
+    api_keys = os.environ.get("API_KEYS")
+    if api_key_header in api_keys:
+        return api_key_header
+    raise AuthorizationError()
 
 
 class RecoResponse(BaseModel):
     user_id: int
     items: List[int]
 
+
+add_reco_responses: Dict = {
+    404: {
+        "description": "Error: User or model is unknown",
+        "content": {
+            "application/json": {
+                "example": {"errors": [{"error_key": "string", "error_message": "string", "error_loc": "string"}]}
+            }
+        },
+    },
+    403: {
+        "description": "Error: Invalid or missing API Key",
+        "content": {
+            "application/json": {
+                "example": {"errors": [{"error_key": "string", "error_message": "string", "error_loc": "string"}]}
+            }
+        },
+    },
+}
 
 router = APIRouter()
 
@@ -27,21 +61,22 @@ async def health() -> str:
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
     response_model=RecoResponse,
+    responses=add_reco_responses,
 )
-async def get_reco(
-    request: Request,
-    model_name: str,
-    user_id: int,
-) -> RecoResponse:
+async def get_reco(request: Request, model_name: str, user_id: int, token: str = Security(get_api_key)) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
-
-    # Write your code here
+    k_recs = request.app.state.k_recs
 
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
-    k_recs = request.app.state.k_recs
-    reco = list(range(k_recs))
+    if model_name == "top":
+        reco = list(range(k_recs))
+    elif model_name == "random":
+        reco = random.sample(range(1, 1000), k_recs)
+    else:
+        raise ModelNotFoundError(error_message=f"Model {model_name} not registered")
+
     return RecoResponse(user_id=user_id, items=reco)
 
 
--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,10 +1,16 @@
+import os
 from http import HTTPStatus
 
+from dotenv import find_dotenv, load_dotenv
 from starlette.testclient import TestClient
 
 from service.settings import ServiceConfig
 
+load_dotenv(find_dotenv())
+
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
+MODEL_NAMES = ["top", "random"]
+TOKEN = os.environ.get("API_KEYS")[0]
 
 
 def test_health(
@@ -20,9 +26,10 @@ def test_get_reco_success(
     service_config: ServiceConfig,
 ) -> None:
     user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    for model_name in MODEL_NAMES:
+        path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": TOKEN})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -34,8 +41,45 @@ def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
     user_id = 10**10
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    for model_name in MODEL_NAMES:
+        path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": TOKEN})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
+
+
+def test_get_reco_for_unknown_model(
+    client: TestClient,
+) -> None:
+    model_name = "unfound_model"
+    user_id = 123
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
+    with client:
+        response = client.get(path, headers={"Authorization": TOKEN})
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json()["errors"][0]["error_key"] == "model_not_found"
+
+
+def test_get_reco_for_invalid_token(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    for model_name in MODEL_NAMES:
+        path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
+    with client:
+        response = client.get(path, headers={"Authorization": "abc"})
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json()["errors"][0]["error_key"] == "invalid_api_key"
+
+
+def test_get_reco_for_missing_token(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    for model_name in MODEL_NAMES:
+        path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
+    with client:
+        response = client.get(path)
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json()["errors"][0]["error_key"] == "http_exception"
